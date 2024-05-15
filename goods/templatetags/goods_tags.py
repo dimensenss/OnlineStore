84da8ff67a784de3fbd83a48282f2750d6a2f423
Dmytro Khoma <dimensenss@gmail.com>
2024-05-15 19:38:04 +0300

    add breadcrumbs

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -15,3 +15,16 @@ def load_best_products():
     return DataMixin().get_products_with_previews(
         Product.objects.filter(is_published=True, cat__slug='best').order_by('-price')
     )
+
+
+@register.simple_tag(name='load_same_products')
+def load_same_products(cat_id, product_id):
+    return DataMixin().get_products_with_previews(
+        Product.objects.filter(is_published=True, cat_id=cat_id).order_by('-price').exclude(id=product_id).distinct()
+    )
+
+@register.inclusion_tag('includes/breadcrumbs.html')
+def get_breadcrumbs(category):
+    return {
+        'category': category
+    }
