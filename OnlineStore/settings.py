8c316498effce5f654e541f17a1667b343c5013b
Dmytro Khoma <dimensenss@gmail.com>
2024-05-26 20:11:41 +0300

    make it better

--- OnlineStore/settings.py
+++ OnlineStore/settings.py
@@ -168,11 +168,11 @@ ACCOUNT_EMAIL_REQUIRED=True
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = "sneakers.shop.noreply@gmail.com"
+EMAIL_HOST_PASSWORD = "luhigbaajgfluxmd"
 
 
 LANGUAGE_CODE = 'uk'
